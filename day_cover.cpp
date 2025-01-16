1a7766847d7874a8899ba132ff15d67429dcde82
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-01-16 18:31:21 +0700

    Update towerdefense

--- day_cover.cpp
+++ day_cover.cpp
@@ -4,7 +4,7 @@
 #include <climits>
 
 using namespace std;
-
+/*
 void cover(vector<vector<bool>> & day_off, int n, int m, int count, int day, vector<int> & used,int & MIN){
     if (day > n){
         MIN = min(MIN, count);
@@ -52,6 +52,56 @@ int main(){
         }
     }
     cover(day_off,n,m,0,1,used,MIN);
+    cout << MIN;
+    return 0;
+}
+First solutions: score 50 */
+
+void cover(int target, int now, int day ,int t_day, vector<int> & use_coub, vector<vector<bool>> & canuse, int & MIN){
+    if (MIN != 21)return;
+    if (now > target && day != t_day-1)return;
+    if (day == t_day && now <= target){
+        MIN = now;
+        return ;
+    }
+    for (int i =0;i<use_coub.size();i++){
+        if (canuse[i][day]){
+            if (use_coub[i] == 0){
+                use_coub[i]++;
+                cover(target,now+1,day+1,t_day,use_coub,canuse,MIN);
+                use_coub[i]--;
+            }else{
+                use_coub[i]++;
+                cover(target,now,day+1,t_day,use_coub,canuse,MIN);
+                use_coub[i]--;
+            }
+        }
+    }
+}
+
+int main(){
+    int MIN = 21;
+    int n,m;
+    cin >> n >> m;
+    vector<vector<bool>> vp(m);
+    vector<int> check(m);
+    for (int i = 0;i<m;i++){
+        vector<bool> c(n);
+        int k;
+        cin>>k;
+        vp[i] = c;
+        for (int j =0;j<k;j++){
+            int idx;
+            cin>> idx;
+            vp[i][idx-1] = true;
+        }
+    }
+    int target = 1;
+    for (int i =0;i<m;i++){
+        cover(target,0,0,n,check,vp,MIN);
+        if (MIN != 21)break;
+        target++;
+    }
     cout << MIN;
     return 0;
 }
--- tower_def.cpp
+++ tower_def.cpp
@@ -2,9 +2,32 @@
 #include <vector>
 using namespace std;
 
-void tower_def(vector<int> & contain, vector<int> & p_monster, vector<int> & h_monster, vector<int> & turret, int health){
-    for (int i = contain[4];i<turret.size();i++){
-        
+void tower_def(vector<int> &pos,vector<int> & contain, vector<int> & p_monster, vector<int> & h_monster, vector<int> & turret, int health, int & count){
+    if (contain[4] == turret.size()){
+        count = min(health,count);
+        return ;
+    }
+    if (turret.size() - contain[4] < abs(count - health) && health > count){
+        return;        
+    }
+    int start = max(turret[contain[4]]-contain[3],0);
+    int end = min(turret[contain[4]]+contain[3],contain[0]);
+    bool check = false;
+    for (int i = start;i<=end;i++){
+        if (pos[i] != 0 && h_monster[i] != 0){
+            contain[4]++;
+            h_monster[i]--;
+            check = true;
+            tower_def(pos,contain,p_monster,h_monster,turret,health-1,count);
+            check =false;
+            h_monster[i]++;
+            contain[4]--;
+        }
+    }
+    if (!check){
+        contain[4]++;
+        tower_def(pos,contain,p_monster,h_monster,turret,health,count);
+        contain[4]--;
     }
 }
 
@@ -17,19 +40,26 @@ int main(){
     v[2] = k;
     v[3] = w;
     v[4] = 0;
+    vector<int> position(n);
     vector<int> p_monster(m);
-    vector<int> h_monster(m);
+    vector<int> h_monster(n);
     vector<int> turret(k);
     int health = 0;
     for (int i  = 0;i<m;i++){
         cin>>p_monster[i];
     }
-    for (int i  = 0;i<m;i++){
-        cin>> h_monster[i];
-        h_monster[i] = h_monster[i]*p_monster[i];
-        health+=h_monster[i];
+    for (int i = 0;i<m;i++){
+        position[p_monster[i]]++;
     }
+    for (int i  = 0;i<m;i++){
+        int hp;
+        cin >> hp;
+        h_monster[p_monster[i]]+= hp;
+        health+= hp;
+    }
+    int count = health;
     for (int i = 0;i<k;i++)cin>>turret[i];
-    tower_def(v,p_monster,h_monster,turret,health);
+    tower_def(position,v,p_monster,h_monster,turret,health,count);
+    cout << count;
     return 0;
 }
