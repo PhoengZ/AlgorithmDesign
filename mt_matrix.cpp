c87b294da028aac724a2bb379fe0b221db2031a2
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-01-24 17:38:39 +0700

    Update mt_mattrix solutions

--- mt_matrix.cpp
+++ mt_matrix.cpp
@@ -3,23 +3,38 @@
 
 using namespace std;
 
-vector<int> mt(vector<int> & contain){
-
+int mt(vector<vector<int>>&base, long long n, int minus, long long row, long long col){
+    if (n == 2){
+        return minus == -1 ? base[row-1][col-1] * -1: base[row-1][col-1];
+    }
+    long long val = n/2;
+    if (row <= val && col <= val){
+        return mt(base,val,minus,row,col);
+    }else if (row <= val && col > val){
+        return mt(base,val,minus,col-val,row);
+    }else if (row > val && col <= val){
+        return mt(base,val,minus*-1,row-val,col);
+    }
+    return mt(base,val,minus *-1,col-val,row-val);
 }
 
 int main(){
     int n,m;
     cin >> n >> m;
-    vector<int> base(n);
-    for (int i = 0;i<4;i++)cin >> base[i];
-    vector <int> v = mt();
-    long long value = 1<<n;
+    vector<vector<int>> base(2);
+    for (int i = 0;i<2;i++){
+        for (int j =0;j<2;j++){
+            int d;
+            cin >> d;
+            base[i].push_back(d);
+        }
+    }
+    long long row = 1LL << n;    
     for (int i = 0;i<m;i++){
         long long r,c;
         cin >> r >> c;
-        long long index = (r-1)*value;
-        index+= c;
-        cout << v[index];
+        int a = mt(base,row,1,r,c);
+        cout << a << endl;
     }
     return 0;
 }
