1d7d093952cc43c9f89362a237cc634a9b8e4569
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-01-26 19:13:03 +0700

    Update solutions pareto_frontier

--- pareto_frontier.cpp
+++ pareto_frontier.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 
-vector<int> pareto(int l,int r){
+vector<pair<int,int>> pareto(int l,int r,vector<pair<int,int>>&v){
+    if(l == r){
+        vector<pair<int,int>> r(1);
+        r[0] = v[l];
+        return r;
+    }
+    int mid = l + (r-l)/2;
+    vector<pair<int,int>> left = pareto(l,mid,v);
+    vector<pair<int,int>> right = pareto(mid+1,r,v);
+    int m_y = right[0].second;
+    vector<pair<int,int>> vp;
+    for (int i = 1;i<right.size();i++){
+        if (right[i].second > m_y){
+            m_y = right[i].second;
+        }
+    }
+    for (int i = 0;i<left.size();i++){
+        if (left[i].second > m_y){
+            vp.push_back(left[i]);
+        }
+    }
+    for (int i =0 ;i<right.size();i++){
+        vp.push_back(right[i]);
+    }
+    return vp;
 }
 int main(){
     int n;
     cin >>n;
-    vector<pair<int,int>> vx(n);
-    vector<pair<int,int>> vy(n);
-
+    vector<pair<int,int>> vx(n+1);
+    for (int i = 1;i<=n;i++){
+        int x,y;
+        cin >> x >> y;
+        vx[i] = {x,y};
+    }
+    sort(vx.begin(),vx.end());
+    vector<pair<int,int>> v = pareto(1,n,vx);
+    cout << v.size();
     return 0;
 }
