82a39071e7dd63902e5dcecc867d65e746c9746d
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-01-27 16:28:40 +0700

    Update solutions self_des_sequence, tilling use other code so this code not last version

--- self_des_seq.cpp
+++ self_des_seq.cpp
@@ -8,16 +8,23 @@ int main(){
     v[1] = 1;
     v[2] = 3;
     v[3] = 5;
-    int count = 2;
-    int start = 3;
-    while (true){
-            for (int j = 0;j<count;j++){
-                v.push_back(v.back() + start);
-            }
+    int index = 3;
+    int count = 3;
+    while (v.back() < 2*1e9){
+        if (count < v[index]){
+            v.push_back(index + v.back());
             count++;
-            start++;
+        }else{
+            index++;
         }
-        cout << v.back() << endl;
+    }
+    for (int i =0;i<n;i++){
+        int find;
+        cin >> find;
+        auto low = lower_bound(v.begin(),v.end(),find);
+        int value = low - v.begin();
+        cout << value <<endl;
+    }
     return 0;
 }
 
--- tiling.cpp
+++ tiling.cpp
@@ -1,9 +1,66 @@
-#include <iostream>
+#include <cstdio>
+#include <tuple>
+#include <vector>
 
-using namespace std;
+std::vector<std::tuple<int, int, int>> answers;
 
-int main(){
-    int l,x,y;
-    cin >> l >> x >> y;
-    return 0;
+void gen(int L, int offsetX, int offsetY, int x, int y) {
+  if (L == 2) {
+    int rot;
+    if (!x && !y) {
+      rot = 0;
+    } else if (x && !y) {
+      rot = 1;
+    } else if (!x) {
+      rot = 2;
+    } else {
+      rot = 3;
+    }
+    answers.emplace_back(rot, offsetX, offsetY);
+    return;
+  }
+  int mid = L / 2;
+  if (x < mid & y < mid) {
+    gen(mid, offsetX, offsetY, x, y);
+    gen(2, offsetX + mid - 1, offsetY + mid - 1, 0, 0);
+  } else {
+    gen(mid, offsetX, offsetY, mid - 1, mid - 1);
+  }
+  if (x >= mid && y < mid) {
+    gen(mid, offsetX + mid, offsetY, x - mid, y);
+    gen(2, offsetX + mid - 1, offsetY + mid - 1, 1, 0);
+  } else {
+    gen(mid, offsetX + mid, offsetY, 0, mid - 1);
+  }
+  if (x < mid && y >= mid) {
+    gen(mid, offsetX, offsetY + mid, x, y - mid);
+    gen(2, offsetX + mid - 1, offsetY + mid - 1, 0, 1);
+  } else {
+    gen(mid, offsetX, offsetY + mid, mid - 1, 0);
+  }
+  if (x >= mid && y >= mid) {
+    gen(mid, offsetX + mid, offsetY + mid, x - mid, y - mid);
+    gen(2, offsetX + mid - 1, offsetY + mid - 1, 1, 1);
+  } else {
+    gen(mid, offsetX + mid, offsetY + mid, 0, 0);
+  }
 }
+
+int main() {
+  int L, X, Y;
+  scanf("%d%d%d", &L, &X, &Y);
+  gen(L, 0, 0, X, Y);
+  printf("%lu\n", answers.size());
+  int rot;
+  for (auto &tup : answers) {
+    std::tie(rot, X, Y) = tup;
+    printf("%d %d %d\n", rot, X, Y);
+  }
+  return 0;
+}
+//กอปมา งง 
+/**
+4 1 2
+
+ */
+
