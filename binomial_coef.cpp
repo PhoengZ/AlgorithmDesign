538ef6ab5b352671eaeb42517a3f8cc949d652b9
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-02 19:02:17 +0700

    Update code larger_restaurent

--- binomial_coef.cpp
+++ binomial_coef.cpp
@@ -1,53 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-int f(int idx, int helth,int range, vector<pair<int,int>>&monster, vector<int>&tower){
-    if (idx == tower.size()){
-        return helth;
-    }
-    auto low = lower_bound(monster.begin(),monster.end(),make_pair(tower[idx] - range,0));
-    int a = helth;
-    for (auto i = low;i != monster.end();i++){
-        if (i->first > tower[idx] + range){
-            a = max(f(idx+1,helth,range,monster,tower),a);
-            break;
-        }else{
-            int index= i-monster.begin();
-            if (i->second <= 0)continue;
-            monster[index].second--;
-            a = max(f(idx+1,helth+1,range,monster,tower),helth+1);
-            monster[index].second++;
-        }
-        
-    }
-    return a;
-}
+
 
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);
-    int n,m,k,w;
-    cin >> n >> m >> k  >> w;
-    vector<int> tower(k);
-    vector<pair<int,int>> monster(m);
-    int total = 0;
+    int n,m;
+    cin >> n >> m;
+    vector<int>v(n);
+    for (int i = 0;i<n;i++)cin >> v[i];
+    sort(v.begin(),v.end());
     for (int i =0;i<m;i++){
-        int a;
-        cin >> a;
-        monster[i].first = a;
+        long long q;
+        cin >> q;
+        if (q <= n){cout << 0 <<endl;continue;}
+        long long left = 1;
+        long long right = (q-1)*v[0];
+        while (left < right){
+            long long mid = left + (right - left)/2;
+            long long total = n;
+            for (int j =0;j<n;j++){
+                total+= mid/v[j]; 
+            }
+            if (total >= q)right = mid;
+            else left = mid+1;
+        }
+        cout << right <<endl;
     }
-    for (int i =0;i<m;i++){
-        int h;
-        cin >> h;
-        monster[i].second = h;
-        total+=h;
-    }
-    sort(monster.begin(),monster.end());
-    for (int i =0;i<k;i++)cin >> tower[i];
-    int a = f(0,0,w,monster,tower);
-    cout << total - a;
-    return 0;
 }
--- larger_restaurant_withbsearch.cpp
+++ larger_restaurant_withbsearch.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-long long sum_value(long long check,vector<int> &v){
-    long long sum = 0;
-    for (auto & e:v){
-        sum += check/e;
-    }
-    return sum;
-}
+
 
 int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
     int n,m;
     cin >> n >> m;
-    vector<int> v(n);
-    for (int i =0;i<n;i++)cin >> v[i];
+    vector<int>v(n);
+    for (int i = 0;i<n;i++)cin >> v[i];
     sort(v.begin(),v.end());
     for (int i =0;i<m;i++){
-        long long a;
-        cin >> a;
-        if (a-n <= 0){cout << 0 <<endl;continue;}
-        long long t = a-n;
+        long long q;
+        cin >> q;
+        if (q <= n){cout << 0 <<endl;continue;}
         long long left = 1;
-        long long right = v[0] * t;
+        long long right = (q-1)*v[0];
         while (left < right){
-            long long mid = left + (right-left)/2;
-            long long val = sum_value(mid,v);
-            if (t <= val){
-                right = mid;
-            }else{
-                left = mid+1;
+            long long mid = left + (right - left)/2;
+            long long total = n;
+            for (int j =0;j<n;j++){
+                total+= mid/v[j]; 
             }
+            if (total >= q)right = mid;
+            else left = mid+1;
         }
-        cout << left <<endl;
-        
+        cout << right <<endl;
     }
-    return 0;
 }
