103bdabd28ede8caaf843c50fb350b88ce266d16
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-10 14:28:56 +0700

    Update distance_one solution

--- distance_one.cpp
+++ distance_one.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+long long f(int n,int k, vector<int>&v){
+    if (n < 0)return 1;
+    if (v[n] != 0)return v[n];
+    long long zero = f(n-1,k,v)% 100000007;
+    long long one = f(n-k,k,v)% 100000007;
+    v[n] = (zero + one)%100000007;
+    return v[n];
+}
 
 int main(){
     int n,k;
     cin >> n >> k;
-    
+    vector<int>v(n);
+    v[0] = 2;
+    long long ans = f(n-1,k,v);
+    cout << ans;
     return 0;
 }
