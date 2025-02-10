51c1ce10cc0183cc561a6bf3ee9199a18810aa95
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-10 20:50:28 +0700

    Add solution eating futomaki

--- bitString_wo_101.cpp
+++ bitString_wo_101.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
 
-#define  ll long long
+#define ll long long
 
 using namespace std;
-ll y;
-ll f(ll n, vector<ll>&table){
-    if (n < 0)return 1;
-    if (n == 0){
-        return 2;
-    }
-    if (table[n] != 0)return table[n];
-    ll zero_1;
-    if (n == y)zero_1 = f(n-1,table)%100000007;
-    else zero_1 = f(n-2,table)%100000007;
-    ll one_1 = f(n-1,table)%100000007;
-    table[n] = (zero_1 + one_1)%100000007;
+
+ll f(ll n, vector<ll> &table)
+{
+    if (table[n] != 0 || n == 0)
+        return table[n];
+    ll zero_1 = f(n - 1, table) % 100000007;
+    ll one_1 = f(n - 3, table) % 100000007;
+    ll one_2 = f(n - 4, table) % 100000007;
+    table[n] = (2 * zero_1 - one_1 + one_2) % 100000007;
     return table[n];
 }
 
-int main(){
+int main()
+{
     ll n;
     cin >> n;
-    y = n-1;
-    vector<ll>table(n);
-    table[0] = 2;
-    long long ans = f(n-1,table);
+    vector<ll> table(n+1);
+    table[0] = 0;
+    table[1] = 0;
+    table[2] = 0;
+    table[3] = 1;
+    long long ans = f(n, table);
     cout << ans;
     return 0;
 }
--- eating_futomaki.cpp
+++ eating_futomaki.cpp
@@ -2,19 +2,32 @@
 #include <vector>
 using namespace std;
 
-int f(int left, int right, int n, vector<int>&v, vector<int>&table){
+int f(int left, int right, vector<int>&v, vector<vector<int>>&table){
     if (abs(right-left) == 1){
         return v[right] > v[left] ? v[right]:v[left];
     }
-    if (table[n]!=0)return table[n];
-
+    if (table[left][right] != 0)return table[left][right];
+    int lr = f(left+1,right-1,v,table);
+    int ll = f(left+2,right,v,table);
+    int rr = f(left,right-2,v,table);
+    lr+= v[left] > v[right] ? v[left]:v[right];
+    ll+= v[left] > v[left+1] ? v[left]:v[left+1];
+    rr+= v[right] > v[right-1] ? v[right]:v[right-1];
+    table[left][right] = table[right][left] = max(lr,max(ll,rr));
+    return table[left][right];
 }
 
 int main(){
     int n;
     cin >>n;
     vector<int>v(n);
-    vector<int>table(n);
+    vector<vector<int>>table(n);
+    for (int i = 0;i<n;i++){
+        vector<int>b(n);
+        table[i] = b;
+    }
     for (int i = 0;i<n;i++)cin >> v[i];
+    int ans = f(0,n-1,v,table);
+    cout << ans;
     return 0;
 }
