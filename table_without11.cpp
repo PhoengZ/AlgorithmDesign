2a00ad7ae39f2598b20e5ea3b053a890ac060de9
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-11 07:56:50 +0700

    Update solution table_without11

--- table_without11.cpp
+++ table_without11.cpp
@@ -5,8 +5,26 @@
 
 using namespace  std;
 
+void f(int n, vector<vector<int>>&table){
+    if (n == 0)return ;
+    if (table[0][n] != 0 && table[1][n] != 0)return ;
+    f(n-1,table);
+    table[0][n] = (table[0][n-1]+table[1][n-1])%100000007;
+    table[1][n] = (2*table[0][n-1] + table[1][n-1])%100000007;
+    return;
+}
+
 int main(){
     ll n;
     cin >> n;
+    vector<vector<int>>v(2);
+    vector<int>a(n);
+    v[0] = a;
+    v[1] = a;
+    v[0][0] = 1;
+    v[1][0] = 2;
+    f(n-1,v);
+    int ans = (v[0][n-1] +v[1][n-1])%100000007;
+    cout << ans;
     return 0;
 }
