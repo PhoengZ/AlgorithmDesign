7c2ac2b4d366812aa0a400c6fb79e2fc39739684
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-26 06:45:50 +0700

    Update solution bilboard_bothside (100)

--- bilboard_bothside.cpp
+++ bilboard_bothside.cpp
@@ -2,7 +2,21 @@
 #include <vector>
 using namespace std;
 
-int target;
+int target,tk;
+
+int t(int n, int row, int w, int k, bool choose, vector<vector<vector<int>>>&dp, vector<vector<int>>&table){
+    if (k == 0)return 0;
+    if (n < 0)return 0;
+    if (n == 0)return choose ? table[row][n]:max(table[0][n],table[1][n]);
+    if (dp[row][n][k] != 0)return dp[row][n][k];
+    if (choose){
+        dp[row][n][k] = max(table[row][n] + t(n-w-1,abs(row-1),w,k-1,true,dp,table),t(n-1,row,w,k,choose,dp,table));
+    }else{
+        dp[row][n][k] = max(t(n-1,row,w,k,false,dp,table),max(t(n-w-1,0,w,k-1,true,dp,table) + table[1][n],t(n-w-1,1,w,k-1,true,dp,table) + table[0][n]));
+    }
+    return dp[row][n][k];
+}
+
 
 int f(int n, int row, int w, bool choose, vector<vector<int>>&dp, vector<vector<int>>&table){
     if (n > target)return 0;
@@ -22,12 +36,23 @@ int main(){
     int n,w,k;
     cin >> n >> w >> k;
     target = n-1;
-    vector<vector<int>>table(2,vector<int>(n));
-    vector<vector<int>>dp(2,vector<int>(n));
-    for (int i = 0;i<n;i++)cin >> table[0][i];
-    for (int i = 0;i<n;i++)cin >> table[1][i];
-    int ans1 = f(0,0,w,false,dp,table);
-    int ans2 = f(0,1,w,false,dp,table);
+    tk = k;
+    int ans1,ans2;
+    if (k == n){
+        vector<vector<int>>table(2,vector<int>(n));
+        vector<vector<int>>dp(2,vector<int>(n));
+        for (int i = 0;i<n;i++)cin >> table[0][i];
+        for (int i = 0;i<n;i++)cin >> table[1][i];
+        ans1 = f(0,0,w,false,dp,table);
+        ans2 = f(0,1,w,false,dp,table);
+    }else{
+        vector<vector<int>>table(2,vector<int>(n));
+        vector<vector<vector<int>>>dp(2,vector<vector<int>>(n,vector<int>(k+1)));
+        for (int i = 0;i<n;i++)cin >> table[0][i];
+        for (int i = 0;i<n;i++)cin >> table[1][i];
+        ans1 = t(n-1,0,w,k,false,dp,table);
+        ans2 = t(n-1,1,w,k,false,dp,table);
+    }
     cout << max(ans1,ans2);
     return 0;
 
