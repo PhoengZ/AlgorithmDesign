5044f68add52d2f15bdf3657c9a71ad486b575f0
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-26 06:30:27 +0700

    Update bilboard_bothside (83.333) Version easier to read

--- bilboard_bothside.cpp
+++ bilboard_bothside.cpp
@@ -2,93 +2,33 @@
 #include <vector>
 using namespace std;
 
-int t;
+int target;
 
-int f1(int n, int row, int w, int k, bool h,vector<vector<pair<int,int>>>&table, vector<vector<int>>&v){
-    if (n < 0)return 0;
-    if (n == 0)return v[row][n];
-    if (table[row][n].first != 0)return table[row][n].first;
-    if (k == 0){
-        return 0;
+int f(int n, int row, int w, bool choose, vector<vector<int>>&dp, vector<vector<int>>&table){
+    if (n > target)return 0;
+    if (n == target)return choose ? table[row][n]:max(table[0][n],table[1][n]);
+    if (dp[row][n] != 0)return dp[row][n];
+    if (choose){
+        dp[row][n] = max(table[row][n] + f(n+w+1,abs(row-1),w,true,dp,table),f(n+1,row,w,choose,dp,table));
     }else{
-        int c;
-        if (row == 0)c = f1(n-w-1,1,w,k-1,true,table,v);
-        else c = f1(n-w-1,0,w,k-1,true,table,v);
-        int nc;
-        if (h){
-            nc = f1(n-1,row,w,k,h,table,v);
-        }else if (n == t || !h){
-            int nc_1 = f1(n-1,0,w,k,false,table,v);
-            int nc_2 = f1(n-1,1,w,k,false,table,v);
-            nc = max(nc_1,nc_2);
-        }
-        if (v[row][n] + c > nc && n-w-1 >= 0 && table[(row+1)%2][n-w-1].second < k){
-            table[row][n].first = v[row][n] + c;
-            table[row][n].second = table[(row+1)%2][n-w-1].second + 1;
-        }else if (v[row][n] + c > nc && n-w-1 < 0){
-            table[row][n].first = v[row][n] + c;
-            table[row][n].second = 1;
-        }else{
-            table[row][n].first = nc;
-            if (n == t || ! h){
-                if (table[0][n-1].first == table[1][n-1].first){
-                    table[row][n].second = table[0][n-1].second < table[1][n-1].second ? table[0][n-1].second:table[1][n-1].second;
-                }else{
-                    table[row][n].second = table[0][n-1].first > table[1][n-1].first ? table[0][n-1].second:table[1][n-1].second;
-                }
-            }else if (h){
-                table[row][n].second = table[row][n-1].second;
-            }
-        }
+        dp[row][n] = max(f(n+1,row,w,false,dp,table),max(f(n+w+1,0,w,true,dp,table) + table[1][n],f(n+w+1,1,w,true,dp,table) + table[0][n]));
     }
-    return table[row][n].first;
-}
-
-
-int f(int n, int row, int w, bool h,vector<vector<int>>&table, vector<vector<int>>&v){
-    if (n < 0)return 0;
-    if(n == 0)return v[row][n];
-    if (table[row][n] != 0)return table[row][n];
-    int c;
-    if (row == 0)c = f(n-w-1,1,w,true,table,v);
-    else c = f(n-w-1,0,w,true,table,v);
-    int nc;
-    if (h){
-        nc = f(n-1,row,w,h,table,v);
-    }else if (n == t || !h){
-        int nc_1 = f(n-1,0,w,false,table,v);
-        int nc_2 = f(n-1,1,w,false,table,v);
-        nc = max(nc_1,nc_2);
-    }
-    table[row][n] = max(v[row][n] + c,nc);
-    return table[row][n];
+    return dp[row][n];
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n,w,k;
-    cin >> n >> w >>k;
-    t = n-1;
-    vector<vector<int>>v(2);
-    vector<int>p(n);
-    vector<vector<int>>vp(2);
-    v[0] = v[1] = vp[0] = vp[1] = p;
-    for (int i =0;i<n;i++)cin >> vp[0][i];
-    for (int i =0;i<n;i++)cin >> vp[1][i];
-    if (k == n){
-        int u1 = f(n-1,0,w,true,v,vp);
-        int u2 = f(n-1,1,w,true,v,vp);
-        cout << max(u1,u2);
-    }else{
-        vector<vector<pair<int,int>>>table(2);
-        vector<pair<int,int>>pre(n);
-        table[0] = table[1] = pre;
-        table[0][0].second = 1;
-        table[1][0].second = 1;
-        int u1 = f1(n-1,0,w,k,true,table,vp);
-        int u2 = f1(n-1,1,w,k,true,table,vp);
-        cout << max(u1,u2);
-    }
+    cin >> n >> w >> k;
+    target = n-1;
+    vector<vector<int>>table(2,vector<int>(n));
+    vector<vector<int>>dp(2,vector<int>(n));
+    for (int i = 0;i<n;i++)cin >> table[0][i];
+    for (int i = 0;i<n;i++)cin >> table[1][i];
+    int ans1 = f(0,0,w,false,dp,table);
+    int ans2 = f(0,1,w,false,dp,table);
+    cout << max(ans1,ans2);
     return 0;
+
 }
--- mcs_with_constraint_kadane.cpp
+++ mcs_with_constraint_kadane.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-    
+    // ยังคิดไม่ออก
     return 0;
 }
