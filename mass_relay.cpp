0071fade1ab9fc9f7df9848ccd5b7385c4850441
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-03-20 06:39:17 +0700

    Update solution Mass_relay with kruskal algorithm

--- mass_relay.cpp
+++ mass_relay.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+int fr(int u, vector<int>&parent){
+    while(parent[u] != -1){
+        u = parent[u];
+    }
+    return u;
+}
+
+bool j(int u, int v, vector<int>&parent){
+    int a = fr(u,parent);
+    int b = fr(v,parent);
+    if (a != b){
+        parent[a] = b;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
 int main(){
     int n,m,q;
-    cin >> n >> m >> q;    
+    cin >> n >> m >> q;
+    vector<vector<int>>g(m,vector<int>(3));
+    vector<int>parent(n,-1);
+    vector<int>sol(n);
+    int t = n-1;
+    for (int i  =0;i<m;i++){
+        cin >> g[i][1] >> g[i][2] >> g[i][0];
+    }
+    sort(g.begin(),g.end());
+    for (auto & e:g){
+        if (j(e[1],e[2],parent)){
+            sol[t--] = e[0];
+        }
+    }
+    for (int i = 0;i<q;i++){
+        int e;
+        cin >> e;
+        cout << sol[e] << endl;
+    }
     return 0;
 }
